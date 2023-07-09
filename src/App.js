5da99eab6b6e3b0606b56083bd4d3773b94ee939
VincentS972 <vincentsechi@gmail.com>
2023-07-09 14:45:56 -0400

    changed title and icon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import './App.css';
 import Signup from './Components/Signup';
 import Navbar from './Components/NavBar';
-  import Home from "./Components/Home"
+import Home from "./Components/Home"
 
 
 
 function App() {
   return (
     <div className="App">
-      <Signup />
       <Navbar />
       <Home />
+      <Signup />
     </div>
   );
 }
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
-import picture from '../public/codepic.jpeg'
+import picture from './codepic.jpeg'
 
 function Home() {
   return (
