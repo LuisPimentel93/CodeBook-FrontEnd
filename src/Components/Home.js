2ca806259bbf4b292d44704624533e21022564c1
Fernanda Reis Kennedy <fernandareisdp@gmail.com>
2023-07-11 07:47:55 -0400

    Correcting indentarion, form and footer almost done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,6 +8,10 @@ import Image from 'react-bootstrap/Image'
 import Button from 'react-bootstrap/Button'
 import logo from './codebookbanner.png'
 import picture from './codepic.jpeg'
+import Form from 'react-bootstrap/Form';
+import { Nav } from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
+
 
 function Home() {
   return (
@@ -16,24 +20,23 @@ function Home() {
         <Image style={{ width: "100%",border: "1px solid #E8E9EB", marginTop: "0px", marginBottom: "5px"  }} src={logo}/>
         <Row style={{ borderBottom: "1px solid #E8E9EB", display: "flex", justifyContent: "space-evenly"}} />
         <Row>
+          
           <Col  xs lg="4" style={{ border:"1px solid #E8E9EB", borderRadius:"10px", marginRight: " 50px",marginTop: "20px"}}>
             <Image style={{ width: "425px" ,border: "1px solid #E8E9EB", marginTop: "0px", marginBottom: "5px"  }} src={picture}/>
             <h4 style={{color: " #564256",}}>Today News</h4> 
             <h5>* GitHub research highlights the impact of AI on developers</h5>
             <h6>To gain deeper insights into how new AI tools and existing workflows impact the overall developer experience, GitHub partnered with Wakefield Research to survey 500 developers from enterprise companies across the US.</h6>
           </Col>
-          <Card style={{ width: '800px', display:"inline-block", marginTop: "20px" }}>
-            <Card.Body>
-              <Card.Title>
-                <h4 style={{color: " #564256",}} >Have some questions?</h4>
-               </Card.Title>
-              <Card.Text>
-                Some quick example needs to be able to add a new content.
-              </Card.Text>
-              <Button variant="secondary">Post</Button>
-            </Card.Body>
-          </Card>  
-  
+
+          <Col style={{border:"2px solid #E8E9EB",borderRadius:"10px", marginTop: "20px", marginBottom: "5px", justifyItems:"center"}}>
+          <Form style={{ width: '800px', display:"inline-block", marginTop: "20px", }}>
+            <h4 style={{color: " #564256",}} >Have some questions?</h4>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Control as="textarea" rows={6}></Form.Control>
+                <Button variant="secondary">Post</Button>
+              </Form.Group>
+          </Form>
+          </Col>
           <Row>
             <Col style={{border:"2px solid #E8E9EB",borderRadius:"10px", marginTop: "20px", marginBottom: "5px", justifyItems:"center"}}>
               <h5 style={{ }}> Our comprehensive guide to CSS flexbox layout. This complete guide explains everything about flexbox, focusing on all the different possible properties for the parent element (the flex container) and the child elements (the flex items). It also includes history, demos, patterns, and a browser support chart. </h5>
@@ -109,7 +112,21 @@ function Home() {
          <Pagination.Next />
          <Pagination.Last /> */}
         </Pagination>
+        <Row style={{ borderBottom: "1px solid #E8E9EB", display: "flex", justifyContent: "space-evenly"}} />
       </Container>
+      <Navbar className="main-footer d-flex justify-content-center navbarContainer">
+        <Nav>
+          <Nav.Link href="#">About us</Nav.Link>
+          <Nav.Link href="#">Contact us</Nav.Link>
+          <Nav.Link href="#">Join us</Nav.Link>
+        </Nav>
+        <Nav className="justify-content-end">
+          <Nav.Link href="">
+            finish the footer here 
+          </Nav.Link>
+        </Nav>
+      </Navbar>
+
     </div >
   );
 }
