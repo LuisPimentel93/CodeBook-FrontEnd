440b6a5f86717a9953488807397213fdec92c1c3
VincentS972 <vincentsechi@gmail.com>
2023-07-16 14:21:35 -0400

    test

--- server.js
+++ server.js
@@ -1,39 +1,26 @@
-// const express = require('express')
-// const mongoose = require('mongoose')
-// require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+
+//middlewares
+app.use(express.json())
 
 
-// // const indexRoute = require('./src/index')
-
-// const app = express()
-
-// //middlewares
-// app.use(express.json())
-
-
-// //routes
-// // app.use('/index', indexRoute)
+//routes
 // app.get('/', (req,res) => {
 //     res.send('I AM ALIVE!!!!!')
 // })
-// // db connection
 
-// const PORT = process.env.PORT
+app.get('/')
 
-// app.listen(PORT, console.log(`listening on port ${PORT}`)) // npx nodemon index.js
+// db connection
 
-// module.exports = app;
+const PORT = process.env.PORT
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+app.listen(PORT, console.log(`listening on port ${PORT}`)) // npx nodemon index.js
+
+module.exports = app;
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-
-    <App />
-
-);
