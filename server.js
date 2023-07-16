78a915c63d00cb80e67c01fa52674e70902ab441
VincentS972 <vincentsechi@gmail.com>
2023-07-16 13:31:00 -0400

    Did a server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 require('dotenv').config()
-// const cors = require('cors')
+const cors = require('cors')
 
 // const indexRoute = require('./src/index')
 
