360e9197959615e8c53778ce79d4d9c6fe8a79c9
VincentS972 <vincentsechi@gmail.com>
2023-07-16 13:29:33 -0400

    Did a server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 // const cors = require('cors')
 
-const indexRoute = require('./src/index')
+// const indexRoute = require('./src/index')
 
 const app = express()
 
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 //routes
-app.use('/index', indexRoute)
+// app.use('/index', indexRoute)
 
 // db connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
