0903b58873652ce08b1e5837af676b05a84fed22
VincentS972 <vincentsechi@gmail.com>
2023-07-16 13:06:59 -0400

    Did a server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+
+
+const app = express()
+
+//middlewares
+app.use(express.json())
+
+//routes
+app.use('/index')
+
+// db connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
+
+const PORT = process.env.PORT
+
+app.listen(PORT, console.log(`listening on port ${PORT}`)) // npx nodemon index.js
+
+module.exports = app
