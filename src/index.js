2a9545e5fb2e5a5789745394287963b2af693fc9
VincentS972 <vincentsechi@gmail.com>
2023-07-17 18:59:56 -0400

    Test

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ app.use('./App', appSrc)
 // db connection
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 
 app.listen(PORT, console.log(`listening on port ${PORT}`)) // npx nodemon index.js
 
